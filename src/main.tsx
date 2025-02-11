97fe04f199cbdb2d9583b96abdbe6790b6273adf
Ivan Pakhomov <ip1133@ya.ru>
2025-02-12 00:42:47 +0300

    init: start task

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { PrimeReactProvider } from 'primereact/api'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.scss'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>
+)
